1ce46f2634cee4b67654f4a89079d52d29c831a0
Артемий <rick-glat@mail.ru>
2021-11-28 13:26:19 +0300

    add ArgumentProvider "CSVSource"

--- src/test/java/qaguru/lesson8/MVideoTests.java
+++ src/test/java/qaguru/lesson8/MVideoTests.java
@@ -7,17 +7,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class Binance {
+public class MVideoTests {
     String URL = "https://www.mvideo.ru/";
 
 
     @Test
     @Tag("BLOCKER")
-    @DisplayName("Поиск товара Iphone")
+    @DisplayName("Поиск товара Iphone на сайте МВидео")
     void SearchProductTest() {
         open(URL);
 
@@ -25,14 +26,29 @@ public class Binance {
         $(".ng-star-inserted").shouldHave(Condition.text("iphone"));
     }
 
-    @ValueSource(strings = {"rtx", "iphone"})
+    @ValueSource(strings = {
+            "rtx",
+            "iphone"
+    })
     @Tag("BLOCKER")
-    @DisplayName("Поиск товара rtx")
-    @ParameterizedTest
+    @ParameterizedTest(name = "Поиск товара {0} на сайте МВидео")
     void SearchProductMVideoTest(String searchQuery) {
         open(URL);
 
         $(".input__field").setValue(searchQuery).pressEnter();
         $(".ng-star-inserted").shouldHave(Condition.text(searchQuery));
+    }
+
+    @CsvSource({
+            "rtx, Видеокарта",
+            "iphone, Смартфон"
+    })
+    @Tag("BLOCKER")
+    @ParameterizedTest(name = "Поиск товара {0} на сайте МВидео и проверка наличия текста {1}")
+    void SearchProductMVideo2Test(String searchQuery, String expectedResult) {
+        open(URL);
+
+        $(".input__field").setValue(searchQuery).pressEnter();
+        $(".ng-star-inserted").shouldHave(Condition.text(expectedResult));
     }
 }
