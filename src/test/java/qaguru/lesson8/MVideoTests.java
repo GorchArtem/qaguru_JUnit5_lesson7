9e495950420ec3ca891a038ce5def399521c681b
Артемий <rick-glat@mail.ru>
2021-11-28 21:55:01 +0300

    add ArgumentProvider "MethodSource"

--- src/test/java/qaguru/lesson8/MVideoTests.java
+++ src/test/java/qaguru/lesson8/MVideoTests.java
@@ -1,17 +1,15 @@
 package qaguru.lesson8;
-
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
+import java.util.stream.Stream;
 import static com.codeborne.selenide.Selenide.*;
-
 public class MVideoTests {
     String URL = "https://www.mvideo.ru/";
 
@@ -39,10 +37,29 @@ public class MVideoTests {
         $(".ng-star-inserted").shouldHave(Condition.text(searchQuery));
     }
 
-    @CsvSource({
-            "rtx, Видеокарта",
-            "iphone, Смартфон"
-    })
+    @CsvSource(value = {
+            "rtx| Видеокарта",
+            "iphone| Смартфон"
+    },
+    delimiter = '|')
+    @Tag("BLOCKER")
+    @ParameterizedTest(name = "Поиск товара {0} на сайте МВидео и проверка наличия текста {1}")
+    void SearchProductMVideo1Test(String searchQuery, String expectedResult) {
+        open(URL);
+
+        $(".input__field").setValue(searchQuery).pressEnter();
+        $(".ng-star-inserted").shouldHave(Condition.text(expectedResult));
+    }
+
+
+    static Stream<Arguments> SearchProductMVideo2Test(){
+        return Stream.of(
+                Arguments.of("rtx", "Видеокарта"),
+                Arguments.of("iphone", "Смартфон")
+        );
+    }
+
+    @MethodSource
     @Tag("BLOCKER")
     @ParameterizedTest(name = "Поиск товара {0} на сайте МВидео и проверка наличия текста {1}")
     void SearchProductMVideo2Test(String searchQuery, String expectedResult) {
